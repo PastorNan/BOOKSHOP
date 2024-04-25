d5fa150d444beba778852a0c8f03c618c6a6dca2
PastorNan <ronan.eastmond@gmail.com>
2024-04-25 14:00:50 -0400

    empleting thee front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
+import ShowAll from './Components/ShowAll'
 
 
-
-function App() {
-  
+function App() {  
 
   return (
-    <>
+    <div className='m-5'>
       <h1 className='text-amber-500  text-6xl text-center'>WELCOME TO THE BOOKSHOP</h1>
-    </>
+      <ShowAll/>
+    </div>
   )
 }
 
